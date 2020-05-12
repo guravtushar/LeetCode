cb103c2e2644afdf76e2a502087ecf500af78962
guravtushar <tushar.gurav25@gmail.com>
2020-05-12 16:33:16 +0530

    updated on 12/05/2020

---
+++ May Challenge/Day_12_Single_Element_in_a_Sorted_Array.cpp
@@ -0,0 +1,46 @@
+/*
+You are given a sorted array consisting of only integers where every element appears exactly twice, except for one element which appears exactly once. Find this single element that appears only once.
+
+ 
+
+Example 1:
+
+Input: [1,1,2,3,3,4,4,8,8]
+Output: 2
+Example 2:
+
+Input: [3,3,7,7,10,11,11]
+Output: 10
+ 
+
+Note: Your solution should run in O(log n) time and O(1) space.
+*/
+
+
+class Solution {
+public:
+    int singleNonDuplicate(vector<int>& nums) {
+        int l=0;
+        int h=nums.size()-1;
+        while(l<h){
+            int mid=l+(h-l)/2;
+            if(mid%2==0){
+                if(nums[mid]==nums[mid+1]){
+                    l=mid+2;
+                }
+                else{
+                    h=mid;
+                }
+            }
+            else{
+                if(nums[mid]==nums[mid-1]){
+                    l=mid+1;
+                }
+                else{
+                    h=mid;
+                }
+            }
+        }
+        return nums[l];
+    }
+};
