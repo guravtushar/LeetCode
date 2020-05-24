4564daec4b7821fa510ee72aa72810ded983c5d0
guravtushar <tushar.gurav25@gmail.com>
2020-05-24 18:30:03 +0530

    uploaded on 24/05/2020

---
+++ May Challenge/Day_24_Construct_Binary_Search_Tree_from_Preorder_Traversal.cpp
@@ -0,0 +1,54 @@
+/*
+Return the root node of a binary search tree that matches the given preorder traversal.
+
+(Recall that a binary search tree is a binary tree where for every node, any descendant of node.left has a value < node.val, and any descendant of node.right has a value > node.val.  Also recall that a preorder traversal displays the value of the node first, then traverses node.left, then traverses node.right.)
+
+It's guaranteed that for the given test cases there is always possible to find a binary search tree with the given requirements.
+
+Example 1:
+
+Input: [8,5,1,7,10,12]
+Output: [8,5,10,1,7,null,12]
+
+
+
+Constraints:
+
+1 <= preorder.length <= 100
+1 <= preorder[i] <= 10^8
+The values of preorder are distinct.
+*/
+
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+	TreeNode* insert(TreeNode* root, int val) {
+		if (!root) {
+			return new TreeNode(val);
+		}
+		if (root->val < val) {
+			root->right = insert(root->right, val);
+		}
+		else {
+			root->left = insert(root->left, val);
+		}
+		return root;
+	}
+	TreeNode* bstFromPreorder(vector<int>& preorder) {
+		TreeNode* root = NULL;
+		for (int i = 0; i < preorder.size(); i++) {
+			root = insert(root, preorder[i]);
+		}
+		return root;
+	}
+};
