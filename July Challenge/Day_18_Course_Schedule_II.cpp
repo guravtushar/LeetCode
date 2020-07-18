77fbc062631db3a029b37b7a801a73828da15d16
guravtushar <tushar.gurav25@gmail.com>
2020-07-18 22:41:16 +0530

    initial commit :rocket:

---
+++ July Challenge/Day_18_Course_Schedule_II.cpp
@@ -0,0 +1,65 @@
+/*
+Course Schedule II
+There are a total of n courses you have to take, labeled from 0 to n-1.
+
+Some courses may have prerequisites, for example to take course 0 you have to first take course 1, which is expressed as a pair: [0,1]
+
+Given the total number of courses and a list of prerequisite pairs, return the ordering of courses you should take to finish all courses.
+
+There may be multiple correct orders, you just need to return one of them. If it is impossible to finish all courses, return an empty array.
+
+Example 1:
+Input: 2, [[1,0]]
+Output: [0,1]
+Explanation: There are a total of 2 courses to take. To take course 1 you should have finished
+             course 0. So the correct course order is [0,1] .
+
+Example 2:
+Input: 4, [[1,0],[2,0],[3,1],[3,2]]
+Output: [0,1,2,3] or [0,2,1,3]
+Explanation: There are a total of 4 courses to take. To take course 3 you should have finished both
+             courses 1 and 2. Both courses 1 and 2 should be taken after you finished course 0.
+             So one correct course order is [0,1,2,3]. Another correct ordering is [0,2,1,3] .
+Note:
+
+The input prerequisites is a graph represented by a list of edges, not adjacency matrices. Read more about how a graph is represented.
+You may assume that there are no duplicate edges in the input prerequisites.
+*/
+
+class Solution {
+public:
+	int n;
+	int dfs(int src, vector<int> adj[], vector<int>& visited, stack<int>& s) {
+		visited[src] = 1;
+		for (int i = 0; i < adj[src].size(); i++) {
+			int val = adj[src][i];
+			if (visited[val] == 1 || (visited[val] == 0 && dfs(val, adj, visited, s))) return 1;
+		}
+		visited[src] = -1;
+		s.push(src);
+		return 0;
+	}
+
+	vector<int> findOrder(int numCourses, vector<vector<int>>& prerequisites) {
+		vector<int> ans;
+		int n = numCourses;
+		vector<int> adj[n];
+		for (int i = 0; i < prerequisites.size(); i++) {
+			int a = prerequisites[i][0];
+			int b = prerequisites[i][1];
+			adj[b].push_back(a);
+		}
+		stack<int> s;
+		vector<int> visited(n, 0);
+		for (int i = 0; i < n; i++) {
+			if (visited[i] == 0) {
+				if (dfs(i, adj, visited, s)) return ans;
+			}
+		}
+		while (!s.empty()) {
+			ans.push_back(s.top());
+			s.pop();
+		}
+		return ans;
+	}
+};
